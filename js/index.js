67d74cb5b43ed4da1adbbd46f137da79cfc8c657
AkshayJangir <newrameshjangir@gmail.com>
2024-02-19 10:51:27 +0000

    added GSAP, CC, Services

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const hero_title_one =  new SplitType('#hero_title_top')
+const hero_title_two =  new SplitType('#hero_title_btm')
+
+gsap.to('.char', {
+    y: 0,
+    stagger: 0.05,
+    delay: 0.2,
+    duration: .1
+
+})
